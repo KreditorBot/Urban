32b42a3d3d312b898d1d96f9191d319e978ae1f7
KreditorBot <rodya_zuyakov@mail.ru>
2024-07-17 18:06:22 +1000

    Add files via upload

---
+++ 3quest.py
@@ -0,0 +1,6 @@
+count = 12
+time = 1.5
+name = 'Python'
+time_4_one = (time*60/count)
+
+print ('\n', 'Курс: ',name,', всего задач: ', count,', затрачено часов: ',time,', среднее время выполнения: ', time_4_one, ' минут.', sep='')
