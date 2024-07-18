47fd3271042281e0a55bbce4e847eb1c5dcc2b6f
KreditorBot <rodya_zuyakov@mail.ru>
2024-07-18 20:26:37 +1000

    Add files via upload

--- DinamicTypization.py
+++ DinamicTypization.py
@@ -2,7 +2,7 @@ name = 'Rodion'
 print('Name:',name)
 age = 27
 print('Age:',age)
-new_age=age+1
-print('New age:',new_age)
+age=age+1
+print('New age:',age)
 is_student = True
 print('Is student:', is_student)
