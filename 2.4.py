b28c264e466bdb5b552766acd9682fe6ea40d53f
KreditorBot <rodya_zuyakov@mail.ru>
2024-07-27 19:10:56 +1000

    Add files via upload

--- 2.4.py
+++ 2.4.py
@@ -1,15 +1,17 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-k = 0
 for i in numbers:
+    if i == 1:
+        continue
+    is_prime = True
     for j in range(2, i):
         if i % j == 0:
-            k = k + 1
-    if k == 0 and i != 1:
+            is_prime = False
+            break
+    if is_prime:
         primes.append(i)
-    elif i != 1:
+    else:
         not_primes.append(i)
-        k = 0
 print(primes)
 print(not_primes)
